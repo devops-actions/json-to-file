cd16919cb20c4a51e744cfe01accfc1200b0f936
Rob Bos <raj.bos@gmail.com>
2022-04-18 22:57:09 +0200

    First setup of new stuff

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
+import * as fs from 'fs'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
+    const json: string = core.getInput('json')
+    const fileName: string = core.getInput('fileName')
+
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.info(`Found filename: ${fileName}`)
+    core.info(`Found json: ${json}`)
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
+    
+    fs.writeFileSync(fileName, json)
+    core.info(`File written successfully`)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
