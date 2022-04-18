3e216e742f076bdfabe05f539126f1da45c22c84
Rob Bos <raj.bos@gmail.com>
2022-04-18 23:34:19 +0200

    Adding new params to the test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -20,6 +20,8 @@ test('wait 500 ms', async () => {
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_JSON'] = '{"foo": "bar"}'
+  process.env['INPUT_FILENAME'] = 'test.json'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
