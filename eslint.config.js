3a6a08cf38f56df0e028c076700d576628a474b0
Rob Bos <raj.bos@gmail.com>
2025-12-07 00:14:01 +0100

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-const typescriptEslint = require('@typescript-eslint/eslint-plugin');
-const typescriptParser = require('@typescript-eslint/parser');
-const jest = require('eslint-plugin-jest');
-const github = require('eslint-plugin-github').default;
-const prettier = require('eslint-plugin-prettier');
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import jest from 'eslint-plugin-jest';
+import github from 'eslint-plugin-github';
+import prettier from 'eslint-plugin-prettier';
 
-module.exports = [
+export default [
   {
     ignores: ['dist/**', 'lib/**', 'node_modules/**', '**/*.d.ts']
   },
@@ -41,7 +41,49 @@ module.exports = [
       '@typescript-eslint/explicit-member-accessibility': [
         'error',
         { accessibility: 'no-public' }
+      ],
+      '@typescript-eslint/no-require-imports': 'error',
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      camelcase: 'off',
+      '@typescript-eslint/consistent-type-assertions': 'error',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        { allowExpressions: true }
+      ],
+      '@typescript-eslint/no-array-constructor': 'error',
+      '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-extraneous-class': 'error',
+      '@typescript-eslint/no-for-in-array': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/no-misused-new': 'error',
+      '@typescript-eslint/no-namespace': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-unnecessary-qualifier': 'error',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-useless-constructor': 'error',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/prefer-for-of': 'warn',
+      '@typescript-eslint/prefer-function-type': 'warn',
+      '@typescript-eslint/prefer-includes': 'error',
+      '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+      '@typescript-eslint/promise-function-async': 'error',
+      '@typescript-eslint/require-array-sort-compare': 'error',
+      '@typescript-eslint/restrict-plus-operands': 'error',
+      semi: 'off',
+      '@typescript-eslint/unbound-method': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto'
+        }
       ]
     }
+  },
+  {
+    files: ['__tests__/**/*.ts'],
+    ...jest.configs['flat/recommended']
   }
 ];
